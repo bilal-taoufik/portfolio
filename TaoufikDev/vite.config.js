f9abce8ed876668b240b86b430ab90d7ba418da7
bilalTaoufik0 <bilal.taoufik@lycee-jeanrostand.fr>
2026-03-20 01:21:00 +0100

    updated

--- TaoufikDev/vite.config.js
+++ TaoufikDev/vite.config.js
@@ -6,7 +6,15 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
-        designguide: resolve(__dirname, "design-guide.html"),
+        ohmycleanservices: resolve(__dirname, "projets/ohmycleanservices.html"),
+        uvitec: resolve(__dirname, "projets/uvitec.html"),
+        vilber: resolve(__dirname, "projets/vilber.html"),
+        vilber3d: resolve(__dirname, "projets/vilber-3d.html"),
+        portfolio: resolve(__dirname, "projets/portfolio.html"),
+        designguide: resolve(__dirname, "projets/design-guide.html"),
+        projets: resolve(__dirname, "projets.html"),
+        contact: resolve(__dirname, "contact.html"),
+        legales: resolve(__dirname, "mentions-legales.html"),
       },
     },
   },
